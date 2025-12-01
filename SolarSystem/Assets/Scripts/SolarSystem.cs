09ba79afc18032500a97c71b6f900ce2fa3144e5
Althea <althea.larrauri@gmail.com>
2025-12-01 18:43:14 +0100

    feat: implementar posiciones y rotaciones de los cuerpos celestes

---
+++ SolarSystem/Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    // Attributes
+    // public
+    public Transform satelite1;
+    public Transform satelite2;
+
+    // private
+    private int _rotationSpeedSol = 30;
+    private int _rotationSpeedSatelite1 = 90;
+    private int _rotationSpeedSatelite2 = 45;
+
+
+    void Start()
+    {
+        transform.localScale = new Vector3(2, 2, 2);
+        transform.position = new Vector3(10, 0, 0);
+
+        satelite1.localScale = new Vector3(1, 1, 1);
+        satelite1.position = new Vector3(5, 0, 0);
+
+        satelite2.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        satelite2.position = new Vector3(2, 0, 0);
+
+    }
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, _rotationSpeedSol * Time.deltaTime);
+        satelite1.RotateAround(transform.position, transform.up, _rotationSpeedSatelite1 * Time.deltaTime);
+        satelite2.RotateAround(satelite1.position, satelite1.right, _rotationSpeedSatelite2 * Time.deltaTime);
+
+
+    }
+}
